09288bd0170f143a888730ae769d53caa4e045c2
matanad <matan19949@gmail.com>
2022-12-28 16:00:01 +0200

    submission - 16:00

--- apps/note/cmps/note-img.jsx
+++ apps/note/cmps/note-img.jsx
@@ -1,4 +1,10 @@
-export function NoteImg({note}){
+const {Fragment} = React
 
-    return <img src={note.info.url} alt={note.info.title} />
+export function NoteImg({note}){
+    const {title, url} = note.info
+
+    return <Fragment>
+        <h1>{title}</h1>
+        <img src={url} alt={title} />
+    </Fragment>
 }

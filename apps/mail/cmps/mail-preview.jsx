17785a4794f4fe42bb5065a4d3dae3b192f4a8d5
Lidor York <yorklidor@gmail.com>
2022-12-28 18:31:52 +0200

    add remove mail option

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,16 +1,23 @@
-
+const { Link } = ReactRouterDOM
+//js
 import { mailService } from "../services/mail.service.js"
+//jsx
+import { LongTxt } from "./long-txt.jsx"
 
-export function MailPreview({mail}){
+export function MailPreview({ mail }) {
 
-    function onMoveToTrash(mail){
+    function onMoveToTrash(mail) {
         mailService.setIsTrash(mail)
     }
-   
+
     return <li className="mail-preview">
-        <h4>{mail.subject}</h4>
-        <h4>{mail.body}</h4>
-        <h4>{mailService.getDate(mail.sentAt)}</h4>
-        <button onClick={()=>onMoveToTrash(mail)}>Move to trash</button>
+        <Link to={`/mail/${mail.id}`}>
+            <h4>{mail.subject}</h4>
+            <LongTxt txt={mail.body} length={10} />
+            <h4>{mailService.getDate(mail.sentAt)}</h4>
+            {mail.isRead && <h4>Mail Read</h4>}
+            {!mail.isRead && <h4>Mail Unread</h4>}
+        </Link>
+        <button onClick={() => onMoveToTrash(mail)}>Move to trash</button>
     </li>
 }
--- apps/mail/views/mail-details.jsx
+++ apps/mail/views/mail-details.jsx
@@ -1,16 +1,49 @@
-import { MailCompose } from "../cmps/mail-compose"
-import { MailSideFiler } from "../cmps/mail-side-filter"
-import { MailTopFilter } from "../cmps/mail-top-filter"
+const { useEffect, useState } = React
+const { useParams, useNavigate } = ReactRouterDOM
+const { Link } = ReactRouterDOM
+
+//js
+import { mailService } from "../services/mail.service.js"
+
+//jsx
+import { MailCompose } from "../cmps/mail-compose.jsx"
+// import { MailSideFiler } from "../cmps/mail-side-filter.jsx"
+// import { MailTopFilter } from "../cmps/mail-top-filter.jsx"
 
 
-export function MailDetails(){
+export function MailDetails() {
     const isComposeOpen = false
+    const [mail, setMail] = useState(null)
+    const params = useParams()
 
+    useEffect(() => {
+        // console.log('params.mailId:', params.mailId)
+        loadMail()
+    }, [])
+
+    function loadMail() {
+        mailService.get(params.mailId)
+            .then((mail) => {
+                mail.isRead = true
+                mailService.save(mail)
+                setMail(mail)
+            })
+            .catch((err) => {
+                console.log('Had issues in mail details', err)
+                navigate('/mail')
+            })
+    }
+
+    if (!mail) return <h1>Loading....</h1>
     return <div>
-        <MailTopFilter/>
-        <MailSideFiler/>
-
-        {isComposeOpen && <MailCompose/>}
-        hi from mail details
+        <h1>hi from mail details</h1>
+        {/* <MailTopFilter/> */}
+        {/* <MailSideFiler/> */}
+        <h4>{mailService.getDate(mail.sentAt)}</h4>
+        <h4>{mail.subject}</h4>
+        <h4>{mail.body}</h4>
+        <button onClick={() => onMoveToTrash(mail)}>Move to trash</button>
+        <Link to='/mail/'>To all Emails</Link>
+        {isComposeOpen && <MailCompose />}
     </div>
 }

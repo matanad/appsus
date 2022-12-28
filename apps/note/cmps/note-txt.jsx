6863655131472b0d534aa526b7de0b7a933d4f5c
matanad <matan19949@gmail.com>
2022-12-29 01:48:09 +0200

    added edit text note

--- apps/note/cmps/note-txt.jsx
+++ apps/note/cmps/note-txt.jsx
@@ -1,4 +1,28 @@
-export function NoteTxt({ note }) {
+const { useRef } = React
 
-    return <p>{note.info.txt}</p>
+import { utilService } from "../../../services/util.service.js"
+
+export function NoteTxt({ note, isDetail = true , saveNote}) {
+    const currNote = useRef(note)
+    const changeDebounce = useRef(utilService.debounce(saveNote))
+
+    function handleChange({currentTarget}) {
+        let { textContent } = currentTarget
+        currNote.current.info.txt = textContent
+        changeDebounce.current(currNote.current)
+    }
+
+    // if (!isDetail) return <div>{note.info.txt}</div>
+    return <div
+        className={isDetail && 'editable'}
+        // type='text'
+        role="textbox"
+        multiline="true"
+        contentEditable={isDetail}
+        spellCheck="true"
+        onInput={handleChange}
+        suppressContentEditableWarning={true}
+    >
+        {note.info.txt}
+    </div>
 }

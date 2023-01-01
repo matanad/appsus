30ddb24af6dd2050581b895abede184aaebc5b43
Lidor York <yorklidor@gmail.com>
2023-01-01 02:34:21 +0200

    add footer

--- views/home.jsx
+++ views/home.jsx
@@ -2,17 +2,26 @@ const { NavLink } = ReactRouterDOM
 
 export function Home() {
 
-    return <section className="home home-page-container flex">
-        <NavLink to="/note">
-            <div className="home-page-img-container">
-                <img src="./assets/img/notesus-main-logo.svg" alt="" />
-            </div>
-        </NavLink>
-        
-        <NavLink to="/mail">
-            <div className="home-page-img-container">
-                <img src="./assets/img/mailsus-main-logo.svg" alt="" />
-            </div>
-        </NavLink>
+    return <section className="home-page-container">
+
+        <div className="logos-container">
+            <NavLink to="/note">
+                <div className="home-page-img-container">
+                    <img src="./assets/img/notesus-main-logo.svg" alt="" />
+                </div>
+            </NavLink>
+
+            <NavLink to="/mail">
+                <div className="home-page-img-container">
+                    <img src="./assets/img/mailsus-main-logo.svg" alt="" />
+                </div>
+            </NavLink>
+        </div>
+
+        <div className="home-footer">
+            <span>
+                &#169; By Matan Adi & Lidor York
+            </span>
+        </div>
     </section>
 }
